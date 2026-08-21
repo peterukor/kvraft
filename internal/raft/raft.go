efdcb0629a908b3c75cea8e4ab9386ce7ba2d12a
Peter Ukor <ukorpeter51@gmail.com>
2026-08-21 01:35:15 -0400

    feat: define Role type for the 3 node states (follower/candidate/leader)

--- internal/raft/raft.go
+++ internal/raft/raft.go
@@ -1,25 +1,40 @@
 package raft
 
+// role type
+type Role int
 
+const (
+	Follower Role = iota
+	Candidate
+	Leader
+)
+
+// log entry
 type LogEntry struct {
-	Index int
-	Term int
+	Index   int
+	Term    int
 	Command string
 }
 
+// raft struct
 type Raft struct {
+	ID          string
 	CurrentTerm int
-	VotedFor string
-	Log []LogEntry
+	Role        Role
+	VotedFor    string
+	Log         []LogEntry
 	CommitIndex int
 	LastApplied int
 }
 
-func NewRaft() *Raft {
-	return &Raft {
+// new raft constructor
+func NewRaft(id string) *Raft {
+	return &Raft{
+		ID:          id,
 		CurrentTerm: 0,
-		VotedFor: "",
-		Log: []LogEntry{},
+		Role:        Follower,
+		VotedFor:    "",
+		Log:         []LogEntry{},
 		CommitIndex: 0,
 		LastApplied: 0,
 	}
