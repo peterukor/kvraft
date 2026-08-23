820c2655634d3b22ac227ec6821b0b477e490faf
Peter Ukor <ukorpeter51@gmail.com>
2026-08-22 21:33:27 -0400

    feat: implement HandleRequestVote RPC handler
    Add lastLogIndexAndTerm helper that retrieves the last index and last term in the log. Also reset VotedFor in HandleRequestVote when a
    higher term is discovered.

--- internal/raft/election.go
+++ internal/raft/election.go
@@ -2,13 +2,15 @@ package raft
 
 import "time"
 
+// struct sent to other nodes to request vote
 type RequestVoteArgs struct {
-	CandidateID   string
-	CandidateTerm int
-	LastLogIndex  int
-	LastLogTerm   int
+	CandidateID           string
+	CandidateTerm         int
+	CandidateLastLogIndex int
+	CandidateLastLogTerm  int
 }
 
+// reply sent for a vote requested
 type RequestVoteReply struct {
 	CurrentTerm int
 	VoteGranted bool
@@ -19,6 +21,23 @@ func (r *Raft) BecomeCandidate() {
 	r.CurrentTerm++
 	r.Role = Candidate
 	r.VotedFor = r.ID
+}
+
+// returns a nodes last log index and term
+func (r *Raft) lastLogIndexAndTerm() (int, int) {
+	var (
+		lastLogIndex int
+		lastLogTerm  int
+	)
+	lastLog := len(r.Log) - 1
+	if lastLog < 0 {
+		lastLogIndex = 0
+		lastLogTerm = 0
+	} else {
+		lastLogIndex = r.Log[lastLog].Index
+		lastLogTerm = r.Log[lastLog].Term
+	}
+	return lastLogIndex, lastLogTerm
 }
 
 // initialize an election countdown timer that resets once the node recieves a heartbeat
@@ -42,23 +61,62 @@ func (r *Raft) RunElectionTimer() {
 	}
 }
 
+// build the requestVoteArgs and returns the address of the struct
 func (r *Raft) BuildRequestVoteArgs() *RequestVoteArgs {
-	var lastIndex int
-	var lastTerm int
 
-	// check if log is empty and initialize to 0 if it is
-	lastLog := len(r.Log) - 1
-	if lastLog < 0 {
-		lastIndex = 0
-		lastTerm = 0
-	} else {
-		lastIndex = r.Log[lastLog].Index
-		lastTerm = r.Log[lastLog].Term
-	}
+	// get last log index and term
+	lastIndex, lastTerm := r.lastLogIndexAndTerm()
+
 	return &RequestVoteArgs{
-		CandidateID:   r.ID,
-		CandidateTerm: r.CurrentTerm,
-		LastLogIndex:  lastIndex,
-		LastLogTerm:   lastTerm,
+		CandidateID:           r.ID,
+		CandidateTerm:         r.CurrentTerm,
+		CandidateLastLogIndex: lastIndex,
+		CandidateLastLogTerm:  lastTerm,
+	}
+}
+
+// handle requestVote request
+func (r *Raft) HandleRequestVote(rv *RequestVoteArgs) *RequestVoteReply {
+	vote := false
+
+	// get last log index and term
+	lastIndex, lastTerm := r.lastLogIndexAndTerm()
+
+	// update current term if candidate term is ahead
+	if rv.CandidateTerm > r.CurrentTerm {
+		r.CurrentTerm = rv.CandidateTerm
+		r.Role = Follower
+		r.VotedFor = ""
+
+		// reject vote if candidate term is behind current term
+	} else if rv.CandidateTerm < r.CurrentTerm {
+		return &RequestVoteReply{
+			CurrentTerm: r.CurrentTerm,
+			VoteGranted: vote,
+		}
+
+		// reject vote if node has voted for a different node in current term
+	} else if (r.VotedFor != rv.CandidateID) && (r.VotedFor != "") {
+		return &RequestVoteReply{
+			CurrentTerm: r.CurrentTerm,
+			VoteGranted: vote,
+		}
+	}
+
+	// compare last log term and last log index
+	if rv.CandidateLastLogTerm > lastTerm {
+		r.VotedFor = rv.CandidateID
+		vote = true
+
+	} else if rv.CandidateLastLogTerm == lastTerm {
+		// compare last log index if last log term matches
+		if rv.CandidateLastLogIndex >= lastIndex {
+			vote = true
+			r.VotedFor = rv.CandidateID
+		}
+	}
+	return &RequestVoteReply{
+		CurrentTerm: r.CurrentTerm,
+		VoteGranted: vote,
 	}
 }
