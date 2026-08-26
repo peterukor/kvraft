65ed303ad2bc968f5cf466b8e907daf52a8f511e
Peter Ukor <ukorpeter51@gmail.com>
2026-08-25 21:30:28 -0400

    feat: randomize election countdown timer
    Add NodeRandomTimer to generate a random election timeout between
    the configured minimum and maximum values.
    
    Use the randomized timeout when the election timer starts and
    whenever it is reset after receiving a heartbeat.
    
    Update the election timer so the node becomes a candidate when the
    timer expires, starts an election, and resets the timer if the
    election is not won.
    
    Start leader replication when the node wins the election.

---
+++ internal/raft/countDownTimer.go
@@ -0,0 +1,78 @@
+package raft
+
+import (
+	"maps"
+	"math/rand/v2"
+	"time"
+)
+
+var electionMin = 800 * time.Millisecond
+var electionMax = 1500 * time.Millisecond
+
+var leaderTimer = 100 * time.Millisecond
+
+func (r *Raft) NodeRandomTimer(min time.Duration, max time.Duration) time.Duration {
+	diff := max - min
+	random := rand.Int64N(int64(diff))
+	return min + time.Duration(random)
+}
+
+// initialize an election countdown timer that resets once the node recieves a heartbeat
+func (r *Raft) RunElectionTimer() {
+	timer := time.NewTimer(r.NodeRandomTimer(electionMin, electionMax))
+	for {
+		select {
+		case <-timer.C:
+
+			r.BecomeCandidate()
+			r.RequestVote()
+			r.mu.Lock()
+			if r.Role == Leader {
+				r.mu.Unlock()
+				r.StartLeaderReplication()
+			} else {
+				r.mu.Unlock()
+				timer.Reset(r.NodeRandomTimer(electionMin, electionMax))
+			}
+
+		case <-r.HeartbeatCh:
+
+			// handle if the timer has fired or is about to fire then reset the timer
+			// returns false if the timer has ended and flushes the channel
+			if !timer.Stop() {
+				select {
+				case <-timer.C:
+				default:
+				}
+			}
+			timer.Reset(r.NodeRandomTimer(electionMin, electionMax))
+		}
+	}
+}
+
+func (r *Raft) StartLeaderReplication() {
+	// build the map
+	r.mu.Lock()
+	myTerm := r.CurrentTerm
+	peersMap := make(map[string]*Peer, len(r.Peers))
+	maps.Copy(peersMap, r.Peers)
+	r.mu.Unlock()
+
+	// start Go routines that will run throughout leadership term
+	for _, peer := range peersMap {
+		go func(peer *Peer) {
+			// keeps Go routine up
+			for {
+				r.mu.Lock()
+				// check if it's still leader or term has changed
+				if r.Role != Leader || r.CurrentTerm != myTerm{
+					r.mu.Unlock()
+					break
+				}
+				r.mu.Unlock()
+				r.AppendEntries(peer)
+				time.Sleep(leaderTimer)
+			}
+		}(peer)
+	}
+}
